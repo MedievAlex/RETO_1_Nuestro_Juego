3b36d327a9237b76122dd0ec4a235ef9da7985a3
Ekasestao <ekaitzcampo2002@gmail.com>
2025-10-22 10:08:57 +0200

    Modificando script

--- RETO 1 - Nuestro Juego/Assets/Scripts/Menu/PauseManager.cs
+++ RETO 1 - Nuestro Juego/Assets/Scripts/Menu/PauseManager.cs
@@ -13,7 +13,6 @@ public class PauseManager : MonoBehaviour
 
     void Start()
     {
-        // Asegurar que hay un EventSystem en la escena
         EnsureEventSystem();
 
         if (Canvas != null)
@@ -31,7 +30,6 @@ public class PauseManager : MonoBehaviour
             GameObject eventSystem = new("EventSystem");
             eventSystem.AddComponent<EventSystem>();
             eventSystem.AddComponent<StandaloneInputModule>();
-            Debug.Log("EventSystem creado para esta escena");
         }
         else if (eventSystems.Length > 1)
         {
@@ -39,7 +37,6 @@ public class PauseManager : MonoBehaviour
             {
                 Destroy(eventSystems[i].gameObject);
             }
-            Debug.Log($"Eliminados {eventSystems.Length - 1} EventSystems duplicados");
         }
     }
 
@@ -83,20 +80,17 @@ public class PauseManager : MonoBehaviour
 
     public void ResumeButton()
     {
-        Debug.Log("=== BOTÓN REANUDAR CLICKEADO ===");
         ResumeGame();
     }
 
     public void OptionsButton()
     {
-        Debug.Log("=== BOTÓN OPCIONES CLICKEADO ===");
         Time.timeScale = 1f;
         SceneManager.LoadScene(optionsMenuScene);
     }
 
     public void MenuButton()
     {
-        Debug.Log("=== BOTÓN MENÚ CLICKEADO ===");
         Time.timeScale = 1f;
         SceneManager.LoadScene(menuScene);
     }
